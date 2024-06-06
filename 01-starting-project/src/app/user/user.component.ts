0b9033021df6e13a670bb3af27ef355a3876d3c5
Eduardo Escamilla <eduardo.escamillasal@gmail.com>
2024-06-06 10:23:14 -0600

    Initial Commit

---
+++ 01-starting-project/src/app/user/user.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, Output,input,output, computed, signal, EventEmitter } from '@angular/core';
+import { type User } from './user.model';
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// };
+
+
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  @Input({ required: true }) user!: User;
+  @Output() select = new EventEmitter<string>();
+  // select = output<string>();
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar ;
+  }
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // })
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+}
